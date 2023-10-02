1c3e68faaf7b14090d2e4dfcf24c2c891b940e01
sachinijayasundara <91739119+sachinijayasundara@users.noreply.github.com>
2023-10-02 16:22:43 +0530

    Create add doctor page in admin view

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
+
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,8 +15,6 @@ use App\Http\Controllers\HomeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-
 
 Route::get('/', [HomeController::class, 'index']);
 
