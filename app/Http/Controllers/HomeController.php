fd69bac6cb07cf5083e34273437c4faec2e38d9d
sachinijayasundara <91739119+sachinijayasundara@users.noreply.github.com>
2023-10-16 17:25:40 +0530

    create table in appointment

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Doctor;
-
+use App\Models\Appointment;
 
 class HomeController extends Controller
 {
@@ -37,5 +37,26 @@ class HomeController extends Controller
 
             return view('user.home', compact('doctor'));
         }
+    }
+    public function appointment(Request $request)
+    {
+        $data = new appointment;
+
+        $data->name = $request->name;
+
+        $data->email = $request->email;
+
+        $data->date = $request->date;
+
+        $data->phone = $request->number;
+
+        $data->message = $request->message;
+
+        $data->doctor = $request->doctor;
+        if (Auth::id()) {
+            $data->user_id = Auth::user()->id;
+        }
+
+        $data->user_id = Auth::user()->id;
     }
 }
