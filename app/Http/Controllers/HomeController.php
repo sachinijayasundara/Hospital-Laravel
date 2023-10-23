c9d39e6db6947272086230ed82a1e7fda29a187b
sachinijayasundara <91739119+sachinijayasundara@users.noreply.github.com>
2023-10-23 12:32:36 +0530

    create cancel appointment

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -65,6 +65,18 @@ class HomeController extends Controller
     }
     public function myappointment()
     {
-        return view('user.my_appointment');
+        if (Auth::id()) {
+            $userid = Auth::user()->id;
+            $appoint = appointment::where('user_id', $userid)->get();
+            return view('user.my_appointment', compact('appoint'));
+        } else {
+            return redirect()->back();
+        }
+    }
+    public function cancel_appoint($id)
+    {
+        $data = appointment::find($id);
+        $data->delete();
+        return redirect()->back()->with('message', 'Your appointment successfully deleted!');
     }
 }
